7c1cdfe1622bd13333320ef5389857475da3f240
sofimarquez22 <sofiamarquez@Sofias-Air.attlocal.net>
2019-09-01 23:04:29 -0700

    fixed errors and the simulation works

--- tipCalculator/tipCalculator/ViewController.swift
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -12,21 +12,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var Percentage: UISegmentedControl!
-    @IBOutlet weak var InputField: UITextField!
+    @IBOutlet weak var percentage: UISegmentedControl!
+    @IBOutlet weak var inputField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func calculateTip(_ sender: Any) {
+    @IBAction func calculate(_ sender: Any) {
         let tipPercent = [0.10, 0.15]
-        let bill = Double(InputField.text!) ?? 0
-        let tip = (bill * tipPercent[Percentage.selectedSegmentIndex])
+        let bill = Double(inputField.text!) ?? 0
+        let tip = (bill * tipPercent[percentage.selectedSegmentIndex])
         let total = tip + bill
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
     
-
 }
 
