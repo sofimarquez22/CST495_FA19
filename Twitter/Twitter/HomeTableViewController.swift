9ad4e759c0fadb3aaf83be56221a7ac7d88470d5
sofimarquez22 <sofiamarquez@Sofias-MacBook-Air.local>
2019-10-09 16:58:54 -0700

    added retweet and like functionality

--- Twitter/Twitter/HomeTableViewController.swift
+++ Twitter/Twitter/HomeTableViewController.swift
@@ -19,14 +19,20 @@ class HomeTableViewController: UITableViewController {
         super.viewDidLoad()
         loadTweet()
         myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
-        tableView.refreshControl = myRefreshControl
+        self.tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+    }
+    override func viewDidAppear(_ animated: Bool){
+        super.viewDidAppear(animated)
+        self.loadTweet()
     }
     @objc func loadTweet(){
         numberOfTweet = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count":numberOfTweet]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success:
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success:
             {(tweets: [NSDictionary]) in
             self.tweetArray.removeAll()
             for tweet in tweets{
@@ -41,10 +47,10 @@ class HomeTableViewController: UITableViewController {
     
     func loadMoreTweets(){
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        numberOfTweet = numberOfTweet + 20
+        numberOfTweet = numberOfTweet + 10
         let myParams = ["count":numberOfTweet]
 
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success:
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success:
             {(tweets: [NSDictionary]) in
                 self.tweetArray.removeAll()
                 for tweet in tweets{
@@ -77,6 +83,9 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell;
     }
     
